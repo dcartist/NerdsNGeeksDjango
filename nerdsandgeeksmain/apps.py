b0598a4253a25b970c9133d2869e7694de7d3236
DCartist <pbannerman13@gmail.com>
2019-07-16 09:59:49 -0400

    Restarting

--- nerdsandgeeksmain/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NerdsandgeeksmainConfig(AppConfig):
-    name = 'nerdsandgeeksmain'
---
+++ nng_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NngMainConfig(AppConfig):
+    name = 'nng_main'
