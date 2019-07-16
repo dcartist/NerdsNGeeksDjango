02b55e8a87763c9134d0aaa47c6bc4a3edb4cb55
DCartist <pbannerman13@gmail.com>
2019-07-16 10:29:34 -0400

    finish pyenv

--- nng_django/settings.py
+++ nng_django/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'nng_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nerdsandgeeks',
+        'HOST': 'localhost',
+        'USER': 'nerdgod1',
+        'PASSWORD': 'WkEX5DBn',
     }
 }
 
--- nng_main/models.py
+++ nng_main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #post author
+    author = models.CharField(max_length = 200, default='The Ultimate Leader')
+    title = models.CharField(max_length = 400, default = 'NNG Social')
+    date = models.DateTimeField(auto_now=True, auto_now_add=False)
+    body = models.TextField(max_length = 4000, default = '')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    #Comment AUthor
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length = 300, default='')
+    #Comment Body
+    body = models.CharField(max_length = 400, default='')
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts',)
+
+    def __str__(self):
+        return self.author
